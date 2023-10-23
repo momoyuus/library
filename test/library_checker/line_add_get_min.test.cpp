0689989727495cae635b2a95adb2babc35b18592
momoyuu <sakiyuu.yuums0613@gmail.com>
2023-10-23 17:41:18 +0900

    add line_add_get_min_test.cpp

---
+++ test/library_checker/line_add_get_min.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/line_add_get_min"
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#include "data/lichaotree.hpp"
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    
+    int n,q;
+    cin>>n>>q;
+    dynamic_li_chao_tree<ll> li(-1e9,1e9,9e18);
+    for(int i = 0;i<n;i++){
+        ll a,b;
+        cin>>a>>b;
+        li.add_line(a,b);
+    }
+    while(q--){
+        int op;
+        cin>>op;
+        if(op==0){
+            ll a,b;
+            cin>>a>>b;
+            li.add_line(a,b);
+        }else{
+            ll p;
+            cin>>p;
+            cout<<li.query(p)<<endl;
+        }
+    }
+}
+
