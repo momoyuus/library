95bde0cf2b484d7c6e7a4bf8407d7033968b0676
momoyuu <sakiyuu.yuuma0613@gmail.com>
2023-11-28 19:10:43 +0900

    add 430.test.cpp

---
+++ test/yukicoder/430.test.cpp
@@ -0,0 +1,41 @@
+#define PROBLEM "https://yukicoder.me/problems/no/430"
+
+#include "string/ahocorasick.hpp"
+
+#include<algorithm>
+#include<iostream>
+#include<vector>
+#include<cassert>
+
+using namespace std;
+using ll = long long;
+int e(){
+    return 0;
+}
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    aho_corasick<26,'A',int,e> t;
+    string s;
+    cin>>s;
+    int m;
+    cin>>m;
+    for(int i = 0;i<m;i++){
+        string c;
+        cin>>c;
+        int ni = t.add(c);
+        t[ni]++;
+    }
+    t.build();
+    vector<ll> cnt(t.size(),0);
+    for(int i:t.getbfs()) cnt[i] += cnt[t.fail[i]] + t[i];
+    ll ans = 0;
+    int ni = 0;
+    for(int i = 0;i<s.size();i++){
+        ni = t.move(ni,s[i]);
+        ans += cnt[ni];
+    }
+    cout<<ans<<endl;
+}
